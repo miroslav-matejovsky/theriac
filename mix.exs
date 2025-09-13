ecf8a7e1f82448a54b400399c1dab8da9908cf85
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-09-13 14:46:27 +0200

    introducing catalog/product

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Theriac.MixProject do
   def application do
     [
       mod: {Theriac.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
---
+++ test/support/fixtures/catalog_fixtures.ex
@@ -0,0 +1,23 @@
+defmodule Theriac.CatalogFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Theriac.Catalog` context.
+  """
+
+  @doc """
+  Generate a product.
+  """
+  def product_fixture(attrs \\ %{}) do
+    {:ok, product} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        price: "120.5",
+        title: "some title",
+        views: 42
+      })
+      |> Theriac.Catalog.create_product()
+
+    product
+  end
+end
